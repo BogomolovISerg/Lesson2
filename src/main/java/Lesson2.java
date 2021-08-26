8a4a8ddabee91336c619963c290fe7474475870b
BogomolovISerg <serg@bogomolov.su>
2021-08-26 21:37:57 +0300

    //

--- src/main/java/Lesson2.java
+++ src/main/java/Lesson2.java
@@ -56,7 +56,7 @@ public class Lesson2 {
 
         boolean result = false;
 
-        if(a % 400 == 0 || a % 4 == 0)
+        if(a % 4 == 0 && ( a % 100 != 0) || (a % 400 == 0))
             result = true;
 
         return result;
